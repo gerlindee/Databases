b558986b01ea2f032a766920445c98c24a8c4671
Gerlinde Christine Ciocea <44527090+gerlindee@users.noreply.github.com>
2018-11-21 21:07:21 +0200

    Data deletion

---
+++ laborator_2/2.sql
@@ -0,0 +1,20 @@
+
+DELETE FROM Issue;
+
+DELETE FROM Plays_In;
+
+DELETE FROM Actor;
+
+DELETE FROM Movie_Adaptation;
+
+DELETE FROM Artist;
+
+DELETE FROM Writer;
+
+DELETE FROM Superhero;
+
+DELETE FROM Publisher;
+
+DELETE FROM Team;
+
+DELETE FROM Member_Of;
