b96e6152bef0a334caf2ae1f114b927456cb0a76
Gerlinde Christine Ciocea <44527090+gerlindee@users.noreply.github.com>
2018-12-21 08:16:56 +0200

    Create 2.sql

---
+++ laborator_5/2.sql
@@ -0,0 +1,5 @@
+INSERT INTO Knights VALUES (1,15,'Obi-Wan Kenobi','Human','Blue'), (2,2,'Mace Windu','Human','Purple'), (3,1,'Yoda','Unknown','Green'), (4,5,'Anakin Skywalker','Human','Blue')
+
+INSERT INTO Padawans VALUES (1,14,'Ahsoka Tano','Shili','Togruta'), (2,15,'Count Dooku','Sereno','Serennian'), (3,17,'Luke Skywalker','Tatooine','Human')
+
+INSERT INTO Mentored_By VALUES (1,1,4), (2,2,3), (3,3,3), (4,3,1)
